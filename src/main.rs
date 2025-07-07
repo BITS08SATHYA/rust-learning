fc9f4a39eb091dab6d89f32acf7e427a3004b247
Sathya <neelasathya06@gmail.com>
2025-07-06 22:05:45 -0400

    variable shadowing impl

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 fn main() {
-    
-    let apples = 50;
-    let oranges = 14 + 6;
-    let fruits = apples + oranges;
-    
-    println!("Fruits count is: {}", fruits);
-    
+
+    // let apples = 50;
+    // let oranges = 14 + 6;
+    // let fruits = apples + oranges;
+    //
+    // println!("Fruits count is: {apples} and {oranges} is {fruits}");
+
+    // let mut gym_reps = 15;
+    //
+    // println!("I plan to do {gym_reps} reps");
+    //
+    // gym_reps = 25;
+    //
+    // println!("I now plan to do {gym_reps} reps");
+
+//     Variable shadowing
+    let grams_of_protein = "100.345";
+    let grams_of_protein = 100.345;
+    let grams_of_protein = 100;
+
 }
