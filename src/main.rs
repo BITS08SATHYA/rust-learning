571cb3c8d04252afb3523ee3ad7377d4f48f0a58
Sathya <neelasathya06@gmail.com>
2025-07-10 20:16:29 -0400

    augemented assignment operators impl

--- src/main.rs
+++ src/main.rs
@@ -85,18 +85,22 @@ fn main() {
 //     println!("Miles away: {}", miles_away_int);
 
 //     Math Operations
-    let addition = 5 + 4;
-    let subtraction = 10  - 6;
-    let multiplication = 3 * 4;
+//     let addition = 5 + 4;
+//     let subtraction = 10  - 6;
+//     let multiplication = 3 * 4;
+//
+//     println!("Addition: {addition}, subtraction: {subtraction}, multiplication: {multiplication}");
+//
+//     let floor_division = 5 / 3;
+//     println!("{floor_division}");
+//
+//     let decimal_division = 5.0/3.0;
+//     println!("{decimal_division:.2}");
+//
+//     let remainder = 8 % 2;
+//     println!("{remainder}");
 
-    println!("Addition: {addition}, subtraction: {subtraction}, multiplication: {multiplication}");
-
-    let floor_division = 5 / 3;
-    println!("{floor_division}");
-
-    let decimal_division = 5.0/3.0;
-    println!("{decimal_division:.2}");
-
-    let remainder = 8 % 2;
-    println!("{remainder}");
+    let mut year = 2025;
+    year += 1;
+    println!("The new year is {year}");
 }
