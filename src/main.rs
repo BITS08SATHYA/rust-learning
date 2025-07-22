3239c4dc0e14f3fb0a33ee5a70158726ba14e8dd
Sathya <neelasathya06@gmail.com>
2025-07-22 13:18:24 -0400

    match statement

--- src/main.rs
+++ src/main.rs
@@ -20,17 +20,17 @@ fn main() {
     //
     // println!("I now plan to do {gym_reps} reps");
 
-//     Variable shadowing
-//     let grams_of_protein = "100.345";
-//     let grams_of_protein = 100.345;
-//     let grams_of_protein = 100;
-//
-//     let income = 100000;
-//     println!("THe tax rate is {income} and my tax rate is {TAX_RATE}")
-//
-//     let mile_race_length: Meters = 1600;
-//     let two_mile_race_length: Meters = 3200;
-//     println!("A one mile race is {mile_race_length} and two_mile_race_length is {two_mile_race_length}");
+    //     Variable shadowing
+    //     let grams_of_protein = "100.345";
+    //     let grams_of_protein = 100.345;
+    //     let grams_of_protein = 100;
+    //
+    //     let income = 100000;
+    //     println!("THe tax rate is {income} and my tax rate is {TAX_RATE}")
+    //
+    //     let mile_race_length: Meters = 1600;
+    //     let two_mile_race_length: Meters = 3200;
+    //     println!("A one mile race is {mile_race_length} and two_mile_race_length is {two_mile_race_length}");
 
     // let season = "FALL";
     //
@@ -46,59 +46,59 @@ fn main() {
     // let favorite_beverage = "whisky";
 
 
-//     Data types
-//     Integer
-//     let eight_bit:u8 = 255;
-//
-//
-//     println!("eight_bit: {}", eight_bit);
+    //     Data types
+    //     Integer
+    //     let eight_bit:u8 = 255;
+    //
+    //
+    //     println!("eight_bit: {}", eight_bit);
 
-//     Strings and Raw Strings
-//     println!("Hello String! \"I love you!\" ");
-//
-//     let filepath = "C:\\My Documents\\new\\videos";
-//     println!("{}" , filepath);
-//
-//     let value = -15i16;
-//     println!("{}", value.abs());
-//
-//     let empty_space = "     my content     ";
-//     println!("{}" , empty_space.trim());
-//
-//     println!("{}", value.pow(2));
-//     println!("{}", value.pow(3));
+    //     Strings and Raw Strings
+    //     println!("Hello String! \"I love you!\" ");
+    //
+    //     let filepath = "C:\\My Documents\\new\\videos";
+    //     println!("{}" , filepath);
+    //
+    //     let value = -15i16;
+    //     println!("{}", value.abs());
+    //
+    //     let empty_space = "     my content     ";
+    //     println!("{}" , empty_space.trim());
+    //
+    //     println!("{}", value.pow(2));
+    //     println!("{}", value.pow(3));
 
-//     floats
-//     let pi = 3.14159;
-//     let pi = 3.14159265358979322384f64;
-//     println!("The current value of pi is: {:.4}", pi);
+    //     floats
+    //     let pi = 3.14159;
+    //     let pi = 3.14159265358979322384f64;
+    //     println!("The current value of pi is: {:.4}", pi);
 
     // println!("{}", pi.floor());
     // println!("{}", pi.ceil());
     // println!("{}", pi.round());
 
-//      Casting as keyword
-//     let miles_away = 50;
-//     let miles_away_i8 = miles_away as u8;
-//     let miles_away_int = miles_away as i32;
-//
-//     println!("Miles away: {}", miles_away_int);
+    //      Casting as keyword
+    //     let miles_away = 50;
+    //     let miles_away_i8 = miles_away as u8;
+    //     let miles_away_int = miles_away as i32;
+    //
+    //     println!("Miles away: {}", miles_away_int);
 
-//     Math Operations
-//     let addition = 5 + 4;
-//     let subtraction = 10  - 6;
-//     let multiplication = 3 * 4;
-//
-//     println!("Addition: {addition}, subtraction: {subtraction}, multiplication: {multiplication}");
-//
-//     let floor_division = 5 / 3;
-//     println!("{floor_division}");
-//
-//     let decimal_division = 5.0/3.0;
-//     println!("{decimal_division:.2}");
-//
-//     let remainder = 8 % 2;
-//     println!("{remainder}");
+    //     Math Operations
+    //     let addition = 5 + 4;
+    //     let subtraction = 10  - 6;
+    //     let multiplication = 3 * 4;
+    //
+    //     println!("Addition: {addition}, subtraction: {subtraction}, multiplication: {multiplication}");
+    //
+    //     let floor_division = 5 / 3;
+    //     println!("{floor_division}");
+    //
+    //     let decimal_division = 5.0/3.0;
+    //     println!("{decimal_division:.2}");
+    //
+    //     let remainder = 8 % 2;
+    //     println!("{remainder}");
 
     // let mut year = 2025;
     // year += 1;
@@ -120,24 +120,97 @@ fn main() {
     //
     // println!("{employee:#?}");
 
-    let month_days = 1..31;
-
-    let month_days = 1..=31;
-    println!("Month days: {month_days:?}");
-
-    for number in month_days {
-        println!("{number}")
-    }
-
-    let letters = 'a'..='z';
-
-    // for letter in letters {
-    //     println!("{letter}");
+    // let month_days = 1..31;
+    //
+    // let month_days = 1..=31;
+    // println!("Month days: {month_days:?}");
+    //
+    // for number in month_days {
+    //     println!("{number}")
+    // }
+    //
+    // let letters = 'a'..='z';
+    //
+    // // for letter in letters {
+    // //     println!("{letter}");
+    // // }
+    //
+    // let colors = ["Red", "Green", "Blue"];
+    //
+    // for color in colors {
+    //     println!("{color} is a great color!")
     // }
 
-    let colors = ["Red", "Green", "Blue"];
+    //     open_store("Newport");
+    //     bake_pizze();
+    //     swim_in_profit();
+    //
+    //     let result = square(8);
+    //     println!("{result}");
+    //
+    //     let multiplier = 3;
+    //
+    //     let calculation = {
+    //         let value = 5 + 4;
+    //         value * multiplier
+    //     };
+    //
+    //     println!("{calculation}")
+    // }
+    //
+    // fn square(number: i32) -> i32 {
+    //     number * number
+    // }
+    //
+    // fn mystery(){
+    //     println!("Hello there!");
+    // }
+    //
+    // fn open_store(neighborhood: &str){
+    //     println!("Opening my pizza store {neighborhood}");
+    // }
+    //
+    // fn bake_pizze(){
+    //     println!("Bake my pizza store");
+    // }
+    //
+    // fn swim_in_profit(){
+    //     println!("SO much $$$, so litte time");
+    // }
 
-    for color in colors {
-        println!("{color} is a great color!")
+    // let season = "summer";
+    //
+    // if season == "summer" {
+    //     println!("School's out");
+    // }else if season == "winter" {
+    //     println!("School's closed")
+    // }
+
+    let evaluation = true;
+
+    let value = match evaluation {
+
+        true => {
+            println!("The value is true");
+        }
+        false => {
+            println!("The value is false");
+        }
+
+    };
+
+    // println!("{value}");
+
+}
+
+
+fn even_or_odd(number: i32) {
+    if number % 2 == 0{
+        "even";
+    }else{
+        "odd";
     }
+
+    let result = if number % 2 == 0 {"even"} else {"odd"};
+    println!("The numbe is {result}");
 }
