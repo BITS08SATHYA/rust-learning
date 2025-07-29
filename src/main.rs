1d61f25b48fb94a59cfe5e16f05a68cda0810f14
Sathya <neelasathya06@gmail.com>
2025-07-29 16:29:56 -0400

    dereference operator impl

--- src/main.rs
+++ src/main.rs
@@ -268,13 +268,16 @@ fn main() {
 //      Reference and Borrowing
     let my_stack_value = 2;
     let my_integer_reference = &my_stack_value;
+    println!("{}", *my_integer_reference);
 
-    println!("{}", my_integer_reference);
 
-    let my_heap_value = String::from("Toyota");
-    let mut my_heap_reference = &my_heap_value;
-    
-    println!("{}", my_heap_reference);
+    // let my_heap_value = String::from("Toyota");
+    // let my_heap_reference = &my_heap_value;
+    //
+    // println!("{}", my_heap_reference);
+
+//     Dereference Operator
+
 
     
 }
